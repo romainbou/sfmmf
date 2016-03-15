15f871135947ec68e418d0f3228406a88c0174be
Romain Bouyé <romain@bouye.fr>
2016-03-15 15:35:07 +0000

    Add bash scripts wraping the docker run commands

---
+++ it.sh
@@ -0,0 +1,5 @@
+if [ ! -n "$1" ]; then
+    echo "Please give the image folder as argument"
+else
+    docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device /dev/dri -v $1:/opt/workdir sfmmf
+fi
