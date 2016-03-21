8a636aee5e8aba72b644679137f62b5c60c5b97a
Romain Bouyé <romain@bouye.fr>
2016-03-21 14:31:49 +0000

    remove uid & gid in scripts

--- it.sh
+++ it.sh
@@ -3,5 +3,5 @@
 if [ ! -n "$1" ]; then
     echo "Please give the image folder as argument"
 else
-    docker run -it -u $(id -u):$(id -g) -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $1:/opt/workdir sfmmf
+    docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $1:/opt/workdir sfmmf
 fi
